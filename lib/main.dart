88a2698cc1e7b7def300a1c8e283aff1bae107d2
HosamKsbaa <35602111+HosamKsbaa@users.noreply.github.com>
2021-03-26 20:33:23 +0200

    test 3

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Git Hub Test',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
